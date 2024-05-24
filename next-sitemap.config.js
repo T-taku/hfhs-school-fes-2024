c452dbe81bf7d03b85451d92528f085b80e12e83
T-taku <t-takuscratch@outlook.jp>
2024-05-24 23:27:04 +0900

    [Add] Sitemap生成

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'schoolfes.hfhs-digital.app', // カスタムドメイン
-    generateRobotsTxt: true,
-    sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
-    outDir: './out', // 出力先ディレクトリ
-};
---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://schoolfes.hfhs-digital.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://schoolfes.hfhs-digital.app/cautions',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://schoolfes.hfhs-digital.app/with-children',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
