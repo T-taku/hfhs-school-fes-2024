4b9184b0d81db900fe5087713781c7696d283d5e
Takuma Tateishi <39856622+T-taku@users.noreply.github.com>
2024-05-24 10:50:58 +0900

    Update VisitorIcon.tsx

--- src/components/VisitorIcon.tsx
+++ src/components/VisitorIcon.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export default function Visitor_MenuIcon({content, link}: { content: ReactNode, link:string }){
   return(
     <>
-      <Link href={`${link}`}>
+      <Link href={`${link}`} prefetch={false}>
         <div className="w-36 h-36 md:w-48 md:h-48 lg:w-52 lg:h-52 grid gap-2 shadow bg-primary_color p-4 place-content-center place-items-center text-center rounded-full transition cursor-pointer text-white hover:opacity-85">
           {content}
         </div>
