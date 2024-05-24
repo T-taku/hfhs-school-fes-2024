2174667d806bcd9610c394b0bdfb67e3b1e0d90b
T-taku <t-takuscratch@outlook.jp>
2024-05-24 23:18:42 +0900

    [Add] 設定をきちんと

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://schoolfefs.hfhs-digital.app',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-}
+    siteUrl: 'schoolfes.hfhs-digital.app', // カスタムドメイン
+    generateRobotsTxt: true,
+    sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
+    outDir: './out', // 出力先ディレクトリ
+};
