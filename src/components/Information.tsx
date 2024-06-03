0455cfe25637f8b61b933bf2b99e97d60f6033a9
T-taku <t-takuscratch@outlook.jp>
2024-06-03 16:22:28 +0900

    [Fix] 時間変更

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -13,14 +13,14 @@ export default function Information() {
           </div>
           <div className="p-4 md:p-10 bg-primary_color justify-center">
             <div className="border-primary_text_color border-solid border-2 p-10 soace-y-3">
-              <h3 className="text-xl md:text-2xl hidden md:block">2024年6月7日(金) 生徒開催 10時00分から13時まで</h3>
+              <h3 className="text-xl md:text-2xl hidden md:block">2024年6月7日(金) 生徒開催 10時00分から13時00分まで</h3>
               <h3 className="text-xl md:text-2xl hidden md:block">2024年6月8日(土) 一般開放 9時30分から14時30分まで</h3>
               <h3 className="text-xl md:text-2xl block md:hidden">
                 2024年6月7日(金)
                 <br />
                 生徒開催
                 <br/>
-                10時00分から13時まで
+                10時00分から13時00分まで
               </h3>
               <h3 className="text-xl md:text-2xl block md:hidden">
                 2024年6月8日(土)
